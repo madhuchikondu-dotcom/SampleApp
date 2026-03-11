dbd8a44e12511af98775d715f0243d9ec07ab032
Madhu Chikondu <madhu.chikondu@wavemaker.com>
2026-03-11 20:28:17 +0530

    Code changes with minor bugs

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const { add, divide } = require("./utils");
+const { login } = require("./auth");
+
+console.log("Add:", add(10, "20")); // bug: string + number
+
+console.log("Divide:", divide(10, 0)); // bug: division by zero
+
 const user = login("admin", "1234");
 
 console.log(user.name); // bug: possible null error
--- utils.js
+++ utils.js
@@ -1,1 +1,15 @@
-var a=123;
+function add(a, b) {
+  return a + b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function slowLoop() {
+  for (var i = 0; i < 1000000000; i++) {
+    console.log(i);
+  }
+}
+
+module.exports = { add, divide, slowLoop };

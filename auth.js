628f350caf65762f04759a11e22070abac7a3c65
Madhu Chikondu <madhu.chikondu@wavemaker.com>
2026-03-11 20:39:49 +0530

    Initial commit

--- auth.js
+++ auth.js
@@ -1,9 +1,1 @@
-function login(username, password) {
-  if (username === "admin" && password === "1234") {
-    return { name: "Admin User" };
-  }
-
-  return null;
-}
-
-module.exports = { login };
+let a=123;
--- utils.js
+++ utils.js
@@ -1,15 +1,1 @@
-function add(a, b) {
-  return a + b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-function slowLoop() {
-  for (var i = 0; i < 1000000000; i++) {
-    console.log(i);
-  }
-}
-
-module.exports = { add, divide, slowLoop };
+let b=1234;
